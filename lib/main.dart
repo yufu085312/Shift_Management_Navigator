7ef5dafb596f66a70c078d754969f58f3b7e5640
福原優太 <fuyu0853@icloud.com>
2025-12-30 19:18:43 +0900

    デバッグタグの削除

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: AppConstants.labelAppName,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
